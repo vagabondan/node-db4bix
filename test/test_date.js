51d8f02a10801b3d7189342f4efbd8b99577b054
Vagabondan <Strannix.Vagabondan@gmail.com>
2020-01-05 00:01:10 +0300

    fix mocha tests

--- test/test_date.js
+++ test/test_date.js
@@ -10,10 +10,13 @@ chai.use(require('chai-as-promised'));
 describe("Тестируем модуль utils/date.js",function(){
 
     it("Date возвращает время в ISO формате",()=>{
-        console.log(new Date().toISOLocalDateTime());     
+        expect(new Date().toISOLocalDateTime()).to.match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+0300/);     
     });
     
     it("Date возвращает время в формате {clock, ns}",()=>{
-        console.log(new Date().getClockNs());     
+        const result = new Date().getClockNs();
+        //expect(result).to.have.all.keys('clock', 'ns'); 
+        expect(result).to.have.property('clock').match(/\d{10}/);
+        expect(result).to.have.property('ns').match(/\d{9}/);
     });
 })
